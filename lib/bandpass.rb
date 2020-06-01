a2a9fa89c4e3f10048c158f5401a70fadb95b6db
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-01 18:55:41 +0100

    test(feat): create upper boundary

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -1,6 +1,15 @@
 class Bandpass
 
+  def initialize(lower_boundary, upper_boundary)
+    @lower_boundary = lower_boundary
+    @upper_boundary = upper_boundary
+  end
+
   def filter(soundwave) 
+
+    soundwave.each_with_index do |frequency, index|
+      soundwave[index] = @upper_boundary if frequency > @upper_boundary
+    end
     return soundwave
   end
 end
--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -1,13 +1,19 @@
 require 'bandpass.rb'
 
 describe Bandpass do
+  let(:bandpass) { Bandpass.new(30, 80) }
+
   describe '.filter' do
     it "accepts an array of one frequency, and returns that same array" do
-      expect(subject.filter([10])).to eq([10])
+      expect(bandpass.filter([10])).to eq([10])
     end
 
     it "accepts an array with a range of soundwave frequencies, and returns that same soundwave array" do
-      expect(subject.filter([30, 50, 40, 60, 40])).to eq([30, 50, 40, 60, 40])
+      expect(bandpass.filter([40, 50, 40, 60, 40])).to eq([40, 50, 40, 60, 40])
+    end
+
+    it "takes an array with frequency that exceeds the upper boundary and sets that frequency to the upper boundary" do
+      expect(bandpass.filter([40, 60, 40, 100, 70])).to eq([40, 60, 40, 80, 70])
     end
   end
 end
