2c5452579aef236beb11f21e67da71f2fa9bcb73
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-01 18:44:44 +0100

    feat(test): test Bandpass can return an array with multiple frequencies

--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -2,8 +2,12 @@ require 'bandpass.rb'
 
 describe Bandpass do
   describe '.filter' do
-    it "accepts an array of frequencies, and returns that same array" do
+    it "accepts an array of one frequency, and returns that same array" do
       expect(subject.filter([10])).to eq([10])
+    end
+
+    it "accepts an array with a range of soundwave frequencies, and returns that same soundwave array" do
+      expect(subject.filter([30, 50, 40, 60, 40])).to eq([30, 50, 40, 60, 40])
     end
   end
 end
