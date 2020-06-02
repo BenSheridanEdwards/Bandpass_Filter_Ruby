7c20f4e04e05f41c04cd66b1a23855311be57b14
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-02 14:03:10 +0100

    test: filter can handle frequencies that exceed both boundaries in the same soundwave

--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -4,7 +4,7 @@ describe Bandpass do
   let(:bandpass) { Bandpass.new(30, 80) }
 
   describe '.filter' do
-    it "accepts a soundwave array, that breached no boundries, and returns that same soundwave array" do
+    it "accepts a soundwave array, that breaches no boundries, and returns that same soundwave array" do
       expect(bandpass.filter([40, 60, 40, 60, 40])).to eq([40, 60, 40, 60, 40])
     end
 
@@ -25,6 +25,12 @@ describe Bandpass do
 
       it "converts multiple exceeding frequencies to the boundary" do
         expect(bandpass.filter([20, 60, 20, 60, 40])).to eq([30, 60, 30, 60, 40])
+      end
+    end
+
+    context "when a soundwave contains frequencies that exceed both the upper and lower boundries" do
+      it "converts both frequencies to their respective boundary" do
+        expect(bandpass.filter([20, 100, 40, 60, 40])).to eq([30, 80, 40, 60, 40])
       end
     end
 
